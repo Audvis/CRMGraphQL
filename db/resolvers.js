892e7013a5d391c5ccaeff0b1c55228a5dc62c56
Audvis <artistaudiovisual@gmail.com>
2021-10-17 22:00:04 -0500

    atenticación

--- db/resolvers.js
+++ db/resolvers.js
@@ -1,11 +1,24 @@
 const Usuario = require('../models/Usuario');
 const bcryptjs = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+require('dotenv').config( { path: 'variables.env' });
 
+const crearToken = (usuario, secreta, expiresIn) => {
+    console.log(usuario);
+    const { id, email, nombre, apellido } = usuario;
+
+     return jwt.sign( { id, email, nombre, apellido }, secreta, { expiresIn } ) 
+ 
+}
 
 // Resolvers
 const resolvers = {
     Query: {
-        obtenerCurso: () => "algo"
+        obtenerUsuario: async (_, { token }) => {
+           const usuarioId = await jwt.verify(token, process.env.SECRETA)
+
+           return usuarioId
+        }
     },
     Mutation: {
         nuevoUsuario: async (_, { input }) => {
@@ -32,6 +45,26 @@ const resolvers = {
 
             }
 
+        },
+        autenticarUsuario: async (_,{input}) => {
+            const { email, password } = input;
+          
+            // Si el usuario existe
+            const existeUsuario = await Usuario.findOne({ email });
+            if (!existeUsuario) {
+                throw new Error('El usuario no existe')
+            }
+
+            // Revisar si el password es correcto
+            const passwordCorrecto = await bcryptjs.compare( password, existeUsuario.password);
+            if(!passwordCorrecto){
+                throw new Error('El password es incorrecto')
+            }
+
+            // Crear token
+            return {
+                token: crearToken(existeUsuario, process.env.SECRETA, '24h')
+            }
         }
     }
 }
